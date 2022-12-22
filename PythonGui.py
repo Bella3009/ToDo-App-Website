d001c27107e9342e32f690b2431b587294d80e07
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2022-12-22 21:37:46 +0100

    Update PythonGui.py
    Add the new task to the Tasks.txt file feature

--- PythonGui.py
+++ PythonGui.py
@@ -1,10 +1,22 @@
-import ToDo_Macro
+import ToDo_Macro as tdm
 import PySimpleGUI as psg
 
-label = psg.Text("Type a Task to be added:")
-input_add = psg.InputText(tooltip="Type a task")
+label = psg.Text("Type a task to be added:")
+input_add = psg.InputText(tooltip="Type a task", key="task_todo")
 add_button = psg.Button("Add")
+
 window = psg.Window("My To Do App", layout = [[label],[input_add], [add_button]], size =(1000,250))
 
-window.read()
+while True:
+    event, value = window.read()
+    if event == psg.WIN_CLOSED:
+        break
+    
+    elif event == "Add":
+        new_todo = value["task_todo"] + "\n"
+        new_todo = new_todo.title()
+        
+        with open("Tasks.txt", "a") as file:
+            file.write(new_todo)
+
 window.close()
