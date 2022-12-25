08c132012b46b142aaf8d01902b2cc5e51a7bd5d
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2022-12-25 22:33:43 +0100

    Update PythonGui.py
    Added the complete feature

--- PythonGui.py
+++ PythonGui.py
@@ -1,15 +1,18 @@
 import GUI_Macro as tdm
 import PySimpleGUI as psg
 
+psg.theme("DarkBlue4")
+
 clean_list = [clean.strip("\n") for clean in tdm.get_todos()]
 
 label = psg.Text("Type a task to be added:")
 input_add = psg.InputText(tooltip="Type a task", key="task_todo")
 add_button = psg.Button("Add")
 edit_button = psg.Button("Edit")
-task_list = psg.Listbox(values=clean_list, key="todo", enable_events=True, size=[250,100])
+task_list = psg.Listbox(values=clean_list, key="todo", enable_events=True, size=[100,25])
+complete_button = psg.Button("Complete")
 
-window = psg.Window("My To Do App", layout = [[label],[add_button, input_add], [edit_button, task_list]], size =[1000,1650])
+window = psg.Window("My To Do App", layout = [[label],[add_button, input_add], [edit_button, complete_button, task_list]], size =[1000,1650])
 
 while True:
     event, value = window.read()
@@ -40,6 +43,18 @@ while True:
         
         window["todo"].update(values=clean_list)
     
+    elif event == "Complete":
+        task_complete = value["todo"][0]
+        task_complete = task_complete + "\n"
+        
+        todo = tdm.get_todos()
+        todo.remove(task_complete)
+        todo = tdm.write_todos(todo)
+        clean_list = [clean.strip("\n") for clean in tdm.get_todos()]
+        
+        window["todo"].update(values=clean_list)
+        window["task_todo"].update(value="")
+        
     elif event == "todo":
         task_todo = value["todo"]
         window["task_todo"].update(value=value["todo"][0])
