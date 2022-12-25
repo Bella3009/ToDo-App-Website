618f87a75d6f7fff5663815e6c06b79059340e00
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2022-12-25 21:55:08 +0100

    Update PythonGui.py
    Added the Edit feature

--- PythonGui.py
+++ PythonGui.py
@@ -1,11 +1,15 @@
-import ToDo_Macro as tdm
+import GUI_Macro as tdm
 import PySimpleGUI as psg
+
+clean_list = [clean.strip("\n") for clean in tdm.get_todos()]
 
 label = psg.Text("Type a task to be added:")
 input_add = psg.InputText(tooltip="Type a task", key="task_todo")
 add_button = psg.Button("Add")
+edit_button = psg.Button("Edit")
+task_list = psg.Listbox(values=clean_list, key="todo", enable_events=True, size=[250,100])
 
-window = psg.Window("My To Do App", layout = [[label],[input_add], [add_button]], size =(1000,250))
+window = psg.Window("My To Do App", layout = [[label],[add_button, input_add], [edit_button, task_list]], size =[1000,1650])
 
 while True:
     event, value = window.read()
@@ -13,10 +17,31 @@ while True:
         break
     
     elif event == "Add":
-        new_todo = value["task_todo"] + "\n"
-        new_todo = new_todo.title()
+        new_todo = value["task_todo"].title() + "\n"
         
         with open("Tasks.txt", "a") as file:
             file.write(new_todo)
-
+            
+        clean_list = [clean.strip("\n") for clean in tdm.get_todos()]
+        
+        window["todo"].update(values=clean_list)
+        window["task_todo"].update(value="")
+        
+    elif event == "Edit":
+        task_edit = value["todo"][0]
+        task_edit = task_edit + "\n"
+        new_todo = value["task_todo"].title() +"\n"
+        
+        todo = tdm.get_todos()
+        index = todo.index(task_edit)
+        todo[index] = new_todo
+        todo = tdm.write_todos(todo)
+        clean_list = [clean.strip("\n") for clean in tdm.get_todos()]
+        
+        window["todo"].update(values=clean_list)
+    
+    elif event == "todo":
+        task_todo = value["todo"]
+        window["task_todo"].update(value=value["todo"][0])
+        
 window.close()
