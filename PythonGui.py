28ae44b3f0f5689f42725aab20ce6cf9db42fb6c
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2023-01-01 19:43:22 +0100

    Update PythonGui.py
    Updated the layout of the Gui

--- PythonGui.py
+++ PythonGui.py
@@ -5,14 +5,19 @@ psg.theme("DarkBlue4")
 
 clean_list = [clean.strip("\n") for clean in tdm.get_todos()]
 
-label = psg.Text("Type a task to be added:")
-input_add = psg.InputText(tooltip="Type a task", key="task_todo")
+label = psg.Text("Type a task to be added:", (50,1))
+empty = psg.Text("")
+input_add = psg.InputText(tooltip="Type a task", key="task_todo", size=(25,1))
 add_button = psg.Button("Add")
 edit_button = psg.Button("Edit")
-task_list = psg.Listbox(values=clean_list, key="todo", enable_events=True, size=[100,25])
+task_list = psg.Listbox(values=clean_list, key="todo", enable_events=True, size=[50,20])
 complete_button = psg.Button("Complete")
+exit_button = psg.Button("Exit")
 
-window = psg.Window("My To Do App", layout = [[label],[add_button, input_add], [edit_button, complete_button, task_list]], size =[1000,1650])
+col1 = [[task_list]]
+col2 = [[empty], [edit_button],[complete_button]]
+ 
+window = psg.Window("My To Do App", layout = [[label], [input_add, add_button], [psg.Column(col1, size=(700,1350)), psg.Column(col2, element_justification="c")], [exit_button]], size =[1000,1650], element_justification="c")
 
 while True:
     event, value = window.read()
@@ -31,17 +36,21 @@ while True:
         window["task_todo"].update(value="")
         
     elif event == "Edit":
-        task_edit = value["todo"][0]
-        task_edit = task_edit + "\n"
-        new_todo = value["task_todo"].title() +"\n"
-        
-        todo = tdm.get_todos()
-        index = todo.index(task_edit)
-        todo[index] = new_todo
-        todo = tdm.write_todos(todo)
-        clean_list = [clean.strip("\n") for clean in tdm.get_todos()]
-        
-        window["todo"].update(values=clean_list)
+        try:
+            task_edit = value["todo"][0]
+            task_edit = task_edit + "\n"
+            new_todo = value["task_todo"].title() +"\n"
+            
+            todo = tdm.get_todos()
+            index = todo.index(task_edit)
+            todo[index] = new_todo
+            todo = tdm.write_todos(todo)
+            clean_list = [clean.strip("\n") for clean in tdm.get_todos()]
+            
+            window["todo"].update(values=clean_list)
+            
+        except IndexError:
+            psg.popup("First select an item from the list, type the new task in the text box and then press the 'Edit' button", title="Invalid selection")
     
     elif event == "Complete":
         task_complete = value["todo"][0]
@@ -58,5 +67,8 @@ while True:
     elif event == "todo":
         task_todo = value["todo"]
         window["task_todo"].update(value=value["todo"][0])
+        
+    elif event == "Exit":
+        break
         
 window.close()
